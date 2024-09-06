b1e2e0d11b9bc8eb7030fedd24541bf508902e6e
NathanAsayDong <nathan.dong@enzy.co>
2024-09-06 17:20:56 -0600

    added basic logic for the pieces movements

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -9,8 +9,12 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    private ChessGame.TeamColor pieceColor;
+    private ChessPiece.PieceType type;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -29,14 +33,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return this.pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return this.type;
     }
 
     /**
@@ -47,6 +51,152 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        Collection<ChessMove> emptyCollection = null;
+        //take the current position
+        //get the current type
+        //calculate all directions it could move
+
+
+        return emptyCollection;
     }
+
+    //helper functions that im adding
+    private Collection<ChessMove> returnAllPosibilites(ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> emptyCollection = null;
+        Collection<ChessPosition> possibleEnd = null;
+        int row = myPosition.getRow();
+        int column = myPosition.getColumn();
+        switch (this.type) {
+            case KING:
+                possibleEnd.add(new ChessPosition(row + 1, column + 1));
+                possibleEnd.add(new ChessPosition(row + 1, column));
+                possibleEnd.add(new ChessPosition(row + 1, column - 1));
+                possibleEnd.add(new ChessPosition(row, column + 1));
+                possibleEnd.add(new ChessPosition(row , column - 1));
+                possibleEnd.add(new ChessPosition(row -1 , column + 1));
+                possibleEnd.add(new ChessPosition(row - 1, column));
+                possibleEnd.add(new ChessPosition(row - 1, column - 1));
+                break;
+            case QUEEN:
+                //horizontal logic
+                while (row < 9) {
+                    row++;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                row = myPosition.getRow();
+                while (row > 0) {
+                    row--;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                row = myPosition.getRow();
+                while (column < 9) {
+                    column++;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                column = myPosition.getColumn();
+                while (column > 0) {
+                    column--;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                column = myPosition.getColumn();
+                //diagnal logic
+                while (column > 0 && row > 0) {
+                    column--;
+                    row--;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                row = myPosition.getRow();
+                column = myPosition.getColumn();
+                while (column > 0 && row < 9) {
+                    column--;
+                    row++;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                row = myPosition.getRow();
+                column = myPosition.getColumn();
+                while (column < 9 && row < 9) {
+                    column++;
+                    row++;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                row = myPosition.getRow();
+                column = myPosition.getColumn();
+                while (column < 9 && row > 0) {
+                    column++;
+                    row--;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                break;
+            case BISHOP:
+                //diagnal logic
+                while (column > 0 && row > 0) {
+                    column--;
+                    row--;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                row = myPosition.getRow();
+                column = myPosition.getColumn();
+                while (column > 0 && row < 9) {
+                    column--;
+                    row++;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                row = myPosition.getRow();
+                column = myPosition.getColumn();
+                while (column < 9 && row < 9) {
+                    column++;
+                    row++;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                row = myPosition.getRow();
+                column = myPosition.getColumn();
+                while (column < 9 && row > 0) {
+                    column++;
+                    row--;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                break;
+            case KNIGHT:
+                possibleEnd.add(new ChessPosition(row + 3, column + 2));
+                possibleEnd.add(new ChessPosition(row + 3, column - 2));
+                possibleEnd.add(new ChessPosition(row - 3, column + 2));
+                possibleEnd.add(new ChessPosition(row - 3, column - 2));
+                possibleEnd.add(new ChessPosition(row + 2, column + 3));
+                possibleEnd.add(new ChessPosition(row + 2, column - 3));
+                possibleEnd.add(new ChessPosition(row - 2, column + 3));
+                possibleEnd.add(new ChessPosition(row - 2, column - 3));
+                break;
+            case ROOK:
+                while (row < 9) {
+                    row++;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                row = myPosition.getRow();
+                while (row > 0) {
+                    row--;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                row = myPosition.getRow();
+                while (column < 9) {
+                    column++;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                column = myPosition.getColumn();
+                while (column > 0) {
+                    column--;
+                    possibleEnd.add(new ChessPosition(row, column));
+                }
+                column = myPosition.getColumn();
+                break;
+            case PAWN:
+                possibleEnd.add(new ChessPosition(row + 2, column)); //up two
+                possibleEnd.add(new ChessPosition(row + 1, column)); //up one
+                possibleEnd.add(new ChessPosition(row + 1, column + 1)); //up right
+                possibleEnd.add(new ChessPosition(row - 1, column + 1)); //down right
+                possibleEnd.add(new ChessPosition(row - 1, column - 1)); //down left
+                possibleEnd.add(new ChessPosition(row + 1, column - 1)); //up left
+                break;
+        }
+    }
+
 }
