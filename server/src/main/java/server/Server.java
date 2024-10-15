64f69f8c60b6a7f059e63f6e0c8ed3481427b443
NathanAsayDong <nathan.dong@enzy.co>
2024-10-15 11:26:09 -0600

    dependencies and boiler plate for server endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,7 +10,15 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", this::registerUser);
+        Spark.post("/session", this::loginUser);
+        Spark.delete("/session", this::logoutUser);
 
+        Spark.get("/game", this::getAllGames);
+        Spark.post("/game", this::createGame);
+        Spark.put("/game", this::joinGame);
+
+        Spark.delete("/db", this::clearApplication);
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
@@ -21,5 +29,33 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    public String registerUser(Request req, Response res) {
+        return "User registered";
+    }
+
+    public String loginUser(Request req, Response res) {
+        return "User logged in";
+    }
+
+    public String logoutUser(Request req, Response res) {
+        return "User logged out";
+    }
+
+    public String getAllGames(Request req, Response res) {
+        return "All games";
+    }
+
+    public String createGame(Request req, Response res) {
+        return "Game created";
+    }
+
+    public String joinGame(Request req, Response res) {
+        return "Game joined";
+    }
+
+    public String clearApplication(Request req, Response res) {
+        return "Application cleared";
     }
 }
