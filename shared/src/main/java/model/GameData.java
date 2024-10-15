5d5bb7229522c1cff55b32979d50838239164327
NathanAsayDong <nathan.dong@enzy.co>
2024-10-15 11:34:24 -0600

    created records

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,5 @@
+package model;
+
+import chess.ChessGame;
+
+public record GameData(Integer gameId, String whiteUsername, String blackUsername, String gameName, ChessGame game) {}
---
+++ shared/src/main/java/model/UserData.java
@@ -0,0 +1,12 @@
+package model;
+
+public record UserData(String username, String password, String email) {
+    public UserData {
+        if (username == null || username.isBlank()) {
+            throw new IllegalArgumentException("Username cannot be null or blank");
+        }
+        if (password == null || password.isBlank()) {
+            throw new IllegalArgumentException("Password cannot be null or blank");
+        }
+    }
+}
