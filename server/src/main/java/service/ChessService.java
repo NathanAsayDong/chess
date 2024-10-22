ddf20667bfcd89d157d9505e996970cede12c4d3
NathanAsayDong <nathan.dong@enzy.co>
2024-10-22 13:33:31 -0600

    removed try catch on chess service for cleaner code

--- server/src/main/java/service/ChessService.java
+++ server/src/main/java/service/ChessService.java
@@ -19,65 +19,41 @@ public class ChessService {
     }
 
     public void clear() throws Exception {
-        try {
-            userDao.clear();
-            gameDao.clear();
-        } catch (DataAccessException e) {
-            throw new DataAccessException("Error accessing database");
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
+        userDao.clear();
+        gameDao.clear();
     }
 
     public List<GameData> getAllGames() throws Exception {
-        try {
-            return gameDao.getAllGames();
-        } catch (DataAccessException e) {
-            throw new DataAccessException("Error accessing database");
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
-        }
+        return gameDao.getAllGames();
     }
 
     public Integer createGame(String gameName) throws Exception {
-        try {
-            if (gameName == null || gameName.isEmpty()) {
-                throw new Exception("Error: game name cannot be empty");
-            }
-            return gameDao.createGame(gameName);
-        } catch (DataAccessException e) {
-            throw new DataAccessException("Error accessing database");
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
+        if (gameName == null || gameName.isEmpty()) {
+            throw new Exception("Error: game name cannot be empty");
         }
+        return gameDao.createGame(gameName);
     }
 
     public void joinGame(Integer gameID, ChessGame.TeamColor teamColor, String username) throws Exception {
-        try {
-            GameData game = gameDao.getGameById(gameID);
-            if (game == null) {
-                throw new Exception("Error: game does not exist");
-            }
-            if (teamColor == null) {
-                throw new Exception("Error: team color cannot be null");
-            }
-            if (username == null || username.isEmpty()) {
-                throw new Exception("Error: username cannot be empty");
-            }
-            if ((teamColor == ChessGame.TeamColor.WHITE && game.whiteUsername() != null) || (teamColor == ChessGame.TeamColor.BLACK && game.blackUsername() != null)) {
-                throw new DataAccessException("Error: already taken");
-            }
-            if (teamColor == ChessGame.TeamColor.WHITE) {
-                GameData updatedGame = new GameData(gameID, username, game.blackUsername(), game.gameName(), game.game());
-                gameDao.updateGame(updatedGame);
-            } else {
-                GameData updatedGame = new GameData(gameID, game.whiteUsername(), username, game.gameName(), game.game());
-                gameDao.updateGame(updatedGame);
-            }
-        } catch (DataAccessException e) {
-            throw new DataAccessException("Error accessing database");
-        } catch (Exception e) {
-            throw new Exception(e.getMessage());
+        GameData game = gameDao.getGameById(gameID);
+        if (game == null) {
+            throw new Exception("Error: game does not exist");
+        }
+        if (teamColor == null) {
+            throw new Exception("Error: team color cannot be null");
+        }
+        if (username == null || username.isEmpty()) {
+            throw new Exception("Error: username cannot be empty");
+        }
+        if ((teamColor == ChessGame.TeamColor.WHITE && game.whiteUsername() != null) || (teamColor == ChessGame.TeamColor.BLACK && game.blackUsername() != null)) {
+            throw new DuplicateInfoException("Error: already taken");
+        }
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            GameData updatedGame = new GameData(gameID, username, game.blackUsername(), game.gameName(), game.game());
+            gameDao.updateGame(updatedGame);
+        } else {
+            GameData updatedGame = new GameData(gameID, game.whiteUsername(), username, game.gameName(), game.game());
+            gameDao.updateGame(updatedGame);
         }
     }
 
