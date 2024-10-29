6b206728aa47cb3a3cd223f0e648871758e4e585
NathanAsayDong <nathan.dong@enzy.co>
2024-10-29 09:22:00 -0600

    addid execute update ot sql user dao

--- server/src/main/java/dataaccess/SqlUserDao.java
+++ server/src/main/java/dataaccess/SqlUserDao.java
@@ -1,11 +1,19 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.AuthData;
 import model.UserData;
 
 import java.sql.SQLException;
 
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
 public class SqlUserDao implements UserDao {
+    private static final String userDataTable = "UserData";
+    private static final String userAuthTable = "UserAuth";
+
     @Override
     public AuthData createUser(UserData user) throws DataAccessException {
         return null;
@@ -33,6 +41,29 @@ public class SqlUserDao implements UserDao {
 
     @Override
     public void clear() throws DataAccessException {
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
     }
 
     private void configureDatabase() throws Exception {
