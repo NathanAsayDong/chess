e2427895fd346db9e0ff851c67a425688dee9fa9
NathanAsayDong <nathan.dong@enzy.co>
2024-10-29 09:10:51 -0600

    create game sql dao

--- server/src/main/java/dataaccess/SqlGameDao.java
+++ server/src/main/java/dataaccess/SqlGameDao.java
@@ -1,9 +1,14 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
 
 import java.sql.SQLException;
 import java.util.List;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class SqlGameDao implements GameDao {
     private static final String table = "GameData";
@@ -12,6 +17,14 @@ public class SqlGameDao implements GameDao {
     private static final String blackUsername = "GameData.blackUsername";
     private static final String gameName = "GameData.gameName";
 
+    public SqlGameDao() throws DataAccessException {
+        try {
+            configureDatabase();
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
     @Override
     public List<GameData> getAllGames() throws DataAccessException {
         return List.of();
@@ -19,7 +32,12 @@ public class SqlGameDao implements GameDao {
 
     @Override
     public Integer createGame(String gameName) throws DataAccessException {
-        return 0;
+        try {
+            ChessGame game = new ChessGame();
+            return executeUpdate("INSERT INTO GameData (gameName, game) VALUES (?, ?)", gameName, game.toString());
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     @Override
@@ -35,6 +53,30 @@ public class SqlGameDao implements GameDao {
     @Override
     public void clear() {
 
+    }
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof ChessGame p) ps.setString(i + 1, new Gson().toJson(p));
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
     }
 
     private void configureDatabase() throws Exception {
@@ -57,7 +99,7 @@ public class SqlGameDao implements GameDao {
           `whiteUsername` varchar(256),
           `blackUsername` varchar(256),
           `gameName` varchar(256) NOT NULL,
-          'game'
+          'game' varchar(256) NOT NULL,
           PRIMARY KEY (`gameId`),
           INDEX(gameName),
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
